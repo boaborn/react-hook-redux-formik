b862e23452c716081c6235346b201f8dab17bdcf
Michael Zhao <Michael.Zhao@optimationgroup.com>
2020-03-19 14:59:48 +1300

    Add template components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
 
-function App() {
+import history from '../history'
+import Header from '../components/Header'
+
+const App = () => {
   return (
-    <div className="App">
-      app
+    <div>
+      <Router history={ history }>
+        <div>
+          <Header/>
+        </div>
+      </Router>
     </div>
   )
 }
